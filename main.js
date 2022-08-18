4715eb99250d1b3747dcb0498d34cb7808257495
SolomonKitonyi <kitonyisolomon@gmail.com>
2022-08-18 16:12:46 +0300

    completed assignment

--- main.js
+++ main.js
@@ -3,7 +3,27 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
-
+const modal = document.querySelector("#modal");
+modal.className = "hidden"
+document.querySelectorAll(".like-glyph").forEach(li => li.addEventListener("click",(e) => {
+  mimicServerCall().then(() => {
+    let heart = e.target
+    if(heart.textContent === FULL_HEART) {
+      heart.textContent = EMPTY_HEART;
+      heart.classList.remove("activated-heart")
+    } else {
+    heart.textContent = FULL_HEART
+    heart.classList.add('activated-heart')
+    }
+  })
+  .catch((error) => {
+    modal.textContent = error
+    modal.classList.remove("hidden")
+    setTimeout(() => {
+      modal.classList.add("hidden")
+    },3000)
+  })
+}))
 
 
 
